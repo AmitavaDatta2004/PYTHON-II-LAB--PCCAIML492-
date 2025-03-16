8185934066346ae5f8f53740b1d0fe508c7acd06
Amitava <dattaarup2004@gmail.com>
2025-03-16 22:04:35 +0530

    done

--- Assignment_3/15.py
+++ Assignment_3/15.py
@@ -7,3 +7,6 @@ data = pd.read_csv('d:\\PYTHON\\PYTHON-II-LAB--PCCAIML492-\\Assignment_3\\iris.c
 data.drop(columns=['total_value'], inplace=True)
 print("\nData Frame after Removing Total Value Column:")
 print(data)
+
+# Save the modified DataFrame back to the CSV file
+data.to_csv('d:\\PYTHON\\PYTHON-II-LAB--PCCAIML492-\\Assignment_3\\iris.csv', index=False)
