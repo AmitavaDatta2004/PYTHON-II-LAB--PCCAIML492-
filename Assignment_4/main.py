517bb6b55703cf0ae5b36070f25c14c8ca755fa1
Amitava <dattaarup2004@gmail.com>
2025-04-20 16:47:04 +0530

    done

--- Assignment_4/main.py
+++ Assignment_4/main.py
@@ -1,11 +1,12 @@
-iris_setosa = iris.loc[iris['variety'] == 'setosa']
-iris_versicolor = iris.loc[iris['variety'] == 'versicolor']
-iris_virginica = iris.loc[iris['variety'] == 'virginica']
+iris_setosa = iris.loc[iris['variety'] == 'Setosa']
+iris_versicolor = iris.loc[iris['variety'] == 'Versicolor']
+iris_virginica = iris.loc[iris['variety'] == 'Virginica']
 
 plt.plot(iris_setosa['petal.length'], np.zeros_like(iris_setosa['petal.length']), 'o', label='setosa')
 plt.plot(iris_virginica['petal.length'], np.zeros_like(iris_virginica['petal.length']), 'o', label='virginica')
 plt.plot(iris_versicolor['petal.length'], np.zeros_like(iris_versicolor['petal.length']), 'o', label='versicolor')
-plt.xlabel('petal.length')
+plt.xlabel('Petal Length')
 plt.grid()
 plt.legend()
+plt.ylim(-1, 1)  # <<< important
 plt.show()
